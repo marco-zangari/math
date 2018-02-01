0f92884a3bc66717c532d120075cad576eb22021
Marco Zangari <mjzangari@gmail.com>
2018-01-31 18:48:24 -0800

    adding printing_series

--- sympy/printing_series.py
+++ sympy/printing_series.py
@@ -1,5 +1,7 @@
 """Print an input number into a series."""
 
+from sympy import Symbol, pprint, init_printing
+
 
 def print_series(n):
     """Print the series.
@@ -8,4 +10,13 @@ def print_series(n):
         _       _           _
         2       3           n
     """
+    init_printing(order='rev-lex')
+    x = Symbol('x')
+    series = x
+    for i in range(2, n + 1):
+        series = series + (x**i) / i
+    pprint(series)
 
+if __name__ == '__main__':
+    n = input('Enter the number of terms you want in the series: ')
+    print_series(int(n))
