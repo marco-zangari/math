8beac9cfec810b4b4affdfad293dc982e322d9b0
Marco Zangari <mjzangari@gmail.com>
2018-01-31 18:40:03 -0800

    added new with sympy

---
+++ sympy/printing_series.py
@@ -0,0 +1,11 @@
+"""Print an input number into a series."""
+
+
+def print_series(n):
+    """Print the series.
+
+    x + x**2 + X**3 +... + x**n
+        _       _           _
+        2       3           n
+    """
+
